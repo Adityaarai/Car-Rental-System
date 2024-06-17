d7a2479e02a80da6df37cfcb14ec4bd7d3cc75f2
Aditya Rai <adityarai23bs@gmail.com>
2024-06-17 17:26:29 +0545

    added car images

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+CAR_TYPE = (
+  ('SUV', 'SUV'),
+  ('EV', 'EV'),
+  ('Truck', 'Truck'),
+  ('Sedan', 'Sedan'),
+)
+
+STATUS = (
+  ('Pending', 'Pending'),
+  ('Approved', 'Approved'),
+  ('Paid', 'Paid'),
+  ('Completed', 'Completed'),
+)
+
+AVAILABILITY = (
+  ('Unlisted', 'Unlisted'),
+  ('Booked', 'Booked'),
+  ('Available', 'Available'),
+)
+
 
 # Create your models here.
+class CarDetail(models.Model):
+    renter_name = models.CharField(max_length=100, null=True)
+    renter_contact = models.CharField(max_length=10, null=True)
+    car_type = models.CharField(max_length=100, choices=CAR_TYPE, null=True)
+    car_model = models.CharField(max_length=100, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
+    availability = models.CharField(max_length=20, choices=AVAILABILITY, null=True, default='Available')
+    image = models.ImageField(default='static/images/lambo.jpg', upload_to='static/car_images')
+
+    # display what is shown in the product name
+    def __str__(self):
+        return f'{self.car_model} - {self.car_type} - {self.renter_name} '
+
+# car orders table model
+class CarOrder(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(CarDetail, on_delete=models.DO_NOTHING)
+    rentee = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    start_date = models.DateTimeField(null=True)
+    end_date = models.DateTimeField(null=True)
+    status = models.CharField(max_length=100, choices=STATUS, null=True, default='Pending')
+    
+    # display what is shown in the order name
+    def __str__(self):
+        return f'{self.rentee} - {self.product} - {self.total_price} - {self.status}'
