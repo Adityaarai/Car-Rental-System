20ed57a4f6d6b68ef0bfe46e038b4d0347c8f831
Aditya Rai <adityarai23bs@gmail.com>
2024-06-20 18:46:27 +0545

    orders can be placed and the order is stored in database

--- cars/urls.py
+++ cars/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('cars/', CarsView.as_view(), name='carlisting'),
   path('about/', AboutUsView.as_view(), name='aboutus'),
   path('orders/<int:pk>', CarDetailView.as_view(), name='orders'),
+  path('orders/create', OrderCreateView.as_view(), name='order_create'),
+  path('orders/complete', OrderPlacedView.as_view(), name='booking_complete')
 ]
--- cars/views.py
+++ cars/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import CarDetail, CarOrder, User
+from datetime import datetime
 
 
 # Create your views here.
@@ -20,6 +21,45 @@ class CarDetailView(DetailView):
   template_name = 'main/car_details.html'
   model = CarDetail
   context_object_name = 'detail'
+
+class OrderCreateView(View):
+  template_name = 'main/car_details.html'
+
+  def post(self, request, *args, **kwargs):
+        rentee = User.objects.get(username=request.user.username) 
+
+        renter_contact = request.POST.get('renter_contact')
+        car_model = request.POST.get('car_model')
+        renter_name = request.POST.get('renter_name')
+        booking_start_date = request.POST.get('bookingStartDate')
+        booking_end_date = request.POST.get('bookingEndDate')
+
+        start_date = datetime.strptime(booking_start_date, '%Y-%m-%d').date()
+        end_date = datetime.strptime(booking_end_date, '%Y-%m-%d').date()
+
+        product = CarDetail.objects.get(car_model=car_model, renter_name=renter_name, renter_contact=renter_contact)
+
+        price = product.price
+        duration = (end_date - start_date).days
+        total_price = price * duration
+
+        order = CarOrder.objects.create(
+          product=product,
+          start_date=start_date,
+          end_date=end_date,
+          rentee=rentee,
+          total_price=total_price
+        )
+        order.save()
+
+        messages.success(request, 'Car booked successfully!')
+        return redirect('booking_complete')
+
+class OrderPlacedView(View):
+  template_name = 'main/booking_complete.html'
+
+  def get(self, request, *args, **kwargs):
+    return render(request, self.template_name)
 
 class AboutUsView(View):
   template_name = 'main/about.html'
